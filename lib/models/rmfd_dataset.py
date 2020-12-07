850e0ea4521fed2320a063648ce7514a5ae3856a
Joaquin-aliaga <joaliaga.g@gmail.com>
2020-12-07 20:06:52 -0300

    add rmfd dataset class

---
+++ lib/models/rmfd_dataset.py
@@ -0,0 +1,38 @@
+'''
+code from https://github.com/JadHADDAD92/covid-mask-detector
+'''
+
+import cv2
+import numpy as np
+from torch import long, tensor
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
+
+
+class MaskDataset(Dataset):
+    """ Masked faces dataset
+        0 = 'no mask'
+        1 = 'mask'
+    """
+    def __init__(self, dataFrame,input_size=[112,112]):
+        self.dataFrame = dataFrame
+        
+        self.transformations = Compose([
+            ToPILImage(),
+            Resize((input_size[0], input_size[1])),
+            ToTensor(), # [0, 1]
+        ])
+    
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            raise NotImplementedError('slicing is not supported')
+        
+        row = self.dataFrame.iloc[key]
+        image = cv2.imdecode(np.fromfile(row['image'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        return {
+            'image': self.transformations(image),
+            'mask': tensor([row['mask']], dtype=long), # pylint: disable=not-callable
+        }
+    
+    def __len__(self):
+        return len(self.dataFrame.index)
