be04f2957aedac5d9ed197fb2e832017ae42553b
Joaquin-aliaga <joaliaga.g@gmail.com>
2020-12-19 23:46:27 -0300

    add getcwd into Biopdsn class and passed as arg to maskdataset class

--- lib/data/rmfd_dataset.py
+++ lib/data/rmfd_dataset.py
@@ -11,24 +11,23 @@ from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
 import os
 
 class MaskDataset(Dataset):
-    def __init__(self, dataFrame,input_size=[112,112]):
+    def __init__(self, dataFrame,root,input_size=[112,112]):
         self.dataFrame = dataFrame
+        self.root = root
         
         self.transformations = Compose([
             ToPILImage(),
             Resize((input_size[0], input_size[1])),
             ToTensor(), # [0, 1]
         ])
-        self.wd = os.getcwd()
-    
     
     def __getitem__(self, key):
         if isinstance(key, slice):
             raise NotImplementedError('slicing is not supported')
         
         row = self.dataFrame.iloc[key]
-        source = cv2.imdecode(np.fromfile(self.wd+'/'+row['source'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-        target = cv2.imdecode(np.fromfile(self.wd+'/'+row['target'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        source = cv2.imdecode(np.fromfile(self.root+'/'+row['source'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        target = cv2.imdecode(np.fromfile(self.root+'/'+row['target'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
         #negative = cv2.imdecode(np.fromfile(row['negative'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
         return {
             'source': self.transformations(source),
