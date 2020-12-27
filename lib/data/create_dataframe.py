541b34fb304ba98aa8ea6a5fcbd5b785e67b6980
Joaquin-aliaga <joaliaga.g@gmail.com>
2020-12-27 16:40:19 -0300

    fix dataframe creation in CASIA dataset case

--- lib/data/create_dataframe.py
+++ lib/data/create_dataframe.py
@@ -17,6 +17,7 @@ datasetPath.unlink()
 '''
 
 CASIA_SAMPLE = 400000
+CASIA_MIN_REPEATED_CLASSES = 4000
 
 def create_negatives_column(df):
     negatives = pd.DataFrame()
@@ -84,12 +85,18 @@ if __name__ == '__main__':
     merge['id_name'] = merge['id_name'].astype('category')
     merge['id_class'] = merge['id_name'].cat.codes
 
-    #take a subsample because in CASIA there're many more examples than in RMFD
+    #take a subsample because in CASIA there're near 15M of examples
+    #and train with the complete dataset it will take much time
     if args.use_database == 'CASIA':
+        counts = merge['id_class'].value_counts()
+        
+        # drop classes repeated less than CASIA_MIN_REPEATED_CLASSES
+        merge = merge[~merge['id_class'].isin(counts[counts < CASIA_MIN_REPEATED_CLASSES].index)]
+
         keep_prop = CASIA_SAMPLE/merge.shape[0]
         lose_prop = 1 - keep_prop
-        merge, _ = train_test_split(merge, test_size=lose_prop, random_state=42)
-        print("Number of CASIA identities (class): {}".format(len(merge.id_name.unique())))
+        merge, _ = train_test_split(merge, test_size=lose_prop, random_state=42,stratify= merge.id_class)
+        print("Number of CASIA identities (class): {}".format(len(merge.id_class.unique())))
 
     dfName = './{}_dataframe.pickle'.format(args.use_database)
     print(f'saving Dataframe to: {dfName}')
