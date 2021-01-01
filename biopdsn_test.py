80b86d2cf0a8e82c34d35a2c3273244d202807a0
Joaquin-aliaga <joaliaga.g@gmail.com>
2021-01-01 20:21:19 -0300

    fix dataframe path

--- biopdsn_test.py
+++ biopdsn_test.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     parser.add_argument("-max_epochs","--max_epochs",help="Maximum epochs to train",default=10,type=int)
 
     #model args
-    parser.add_argument("-model_weights","--model_weights",help="Path to model (trained) weights",default=None,type=str)
+
+    parser.add_argument("-model_weights","--model_weights",choices = [""],help="Which model weights use",default=None,type=str)
     parser.add_argument("-i", "--input_size", help="input size", default="3,112,112", type=str)
     parser.add_argument("-e", "--embedding_size", help="embedding size",default=512, type=int)
     parser.add_argument("-rw", "--resnet_weights", help="Path to resnet weights", default="./weights/model-r50-am-lfw/model,00",type=str)
@@ -27,7 +28,7 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
-    args.dfPath = "./lib/data/{}_dataframe-pickle".format(args.test_database)
+    args.dfPath = "./lib/data/BioDBv3/{}_dataframe-pickle".format(args.test_database)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
